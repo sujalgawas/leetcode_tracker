9e83983f004b3264412a307ecf163c2d32a72785
sujal gawas <sujalgawas18@gmail.com>
2026-05-05 15:09:13 +0530

    initial commit with frontend and backend

---
+++ frontend/src/components/nav.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function Nav() {
+  return (
+    <>
+      <div>
+        <h1>testing</h1>
+      </div>
+    </>
+  )
+}
+
+export default Nav
